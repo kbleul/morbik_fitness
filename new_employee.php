07d56651c757f8f47d7bfdc03f18e4b3d4ccf3b0
kbleul <kbleulseged@gmail.com>
2022-04-10 04:41:05 -0700

    manager register  employee build 2

--- new_employee.php
+++ new_employee.php
@@ -2,6 +2,65 @@
     <?php session_start(); 
    if( isset($_SESSION["email"]) == false || isset($_SESSION["password"] ) == false)
    {  echo "<script>location.href = 'unautorizedaction.php';</script>"; }
+
+   include('database_connect.php');
+
+   if(isset($_POST['submit'])) {
+
+    $username = $_POST['username'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $jobtitle = $_POST['job'];
+    $salary = $_POST['salary'];
+    $phone = $_POST['phone'];
+    $program = $_POST['program'];
+    $fee = $_POST['fee'];
+
+    $query = "INSERT INTO employee (FNAME,LName,Gender,DOB,Email,Job_title,Salary) 
+               VALUES ('$fname','$lname','$gender','$dob','$email','$jobtitle','$salary')";
+ echo "<script>console.log('1')</script>";
+    if( mysqli_query($con,$query)) {
+   $query = "SELECT * FROM employee WHERE Email='$email' AND FName='$fname' AND LName='$lname' ";
+   echo "<script>console.log('2')</script>";
+
+   if($result = $con->query($query)){
+ echo "<script>console.log('3')</script>";
+
+    while($row = $result -> fetch_assoc() ){
+        $empid = $row['ID'];
+    
+        $query = "INSERT INTO employee_contact (EmpID,Phone_Number) VALUES ('$empid','$phone')";
+
+    if( mysqli_query($con,$query)) {
+ echo "<script>console.log('4')</script>";
+
+        if(isset($program) && isset($fee) && trim($program) != '' && trim($fee) != '')
+        {
+ echo "<script>console.log('5')</script>";
+
+          $query = "INSERT INTO private_trainer (Eid,Type,Fee_per_hour) VALUES ('$empid','$program','$fee')";
+                  mysqli_query($con,$query);
+        if(isset($username) And trim($username) != '' )
+        { $query = "INSERT INTO important_employees (id,Username) VALUES ('$empid','$username')"; }
+                  mysqli_query($con,$query);
+                
+ echo "<script>console.log('6')</script>";
+
+
+        }
+    } else { echo mysqli_error($con);}
+
+            }
+        } else { echo mysqli_error($con); }
+    }else { echo mysqli_error($con);}
+    
+
+
+   }
+
     ?>
 
 <!DOCTYPE html>
@@ -112,28 +171,27 @@
 
             <h2 class="main_title" >Register New Employee</h2>
                
-            <form class="register_employee-form">
+            <form class="register_employee-form" method="post" name="employee_registration_form">
                 <div>
 
-                    <label for="email">Email</label>
+                    <label for="email">Email *</label>
                     <input type="email" name="email" id="email" required="required" autocomplete="off"
                         placeholder="xyz@mail.com">
                     <label for="username">User Name</label>
                     <input type="text" name="username" id="username">
-                    <label for="fname">First Name</label>
+                    <label for="fname">First Name *</label>
                     <input type="text" name="fname" id="fname" required="required">
-                    <label for="lname">Last Name</label>
+                    <label for="lname">Last Name *</label>
                     <input type="text" name="lname" id="lname" required="required">
-                    <label for="phone">Phone Number</label>
+                    <label for="phone">Phone Number *</label>
                     <input type="tel" name="phone" id="phone" required="required">
-                    <label for="age">Date Of Birth</label>
+                    <label for="age">Date Of Birth *</label>
                     <input type="date" name="dob" id="dob" required="required">
                 </div>
 
                 <div>
 
-                   
-                    <label for="gender" class="" flex>Gender</label>
+                    <label for="gender" class="" flex>Gender *</label>
                     <section class="new_emp-gender--container">
                         <section class="gender_subcontainer">
                             <p>Male</p><input type="radio" value="male" name="gender" id="male">
@@ -142,19 +200,29 @@
                             <p>Female</p><input type="radio" value="female" name="gender" id="female">
                         </section>
                     </section>
-                    <label for="job">Job Title</label>
-                    <input type="text"  name="job" id="job" required="required">
-                    <label for="salary">Salary</label>
+                    <label for="job" class="job">Job Title *</label>
+                    <input type="text"  name="job" id="job" class="job" required="required">
+                    <label for="salary">Salary *</label>
                     <input type="number"  name="salary" id="salary" required="required" >
+                    <section id="for_trainer">
+                        <label for="program">Program</label>
+                        <select name="program" id="program" >
+                            <?php
+                                include('database_connect.php');
 
-                    <label for="program">Program</label>
-                    <select name="program" id="progrma">
-
-                    </select>
-                    <label for="fee">Fee per hour</label>
-                    <input type="number" name="fee" id="fee" >
-
-                    <input type="submit" value="Submit">
+                                $query = "SELECT * FROM main_program";
+                                if($result = $con->query($query)){
+                                    while($row = $result -> fetch_assoc() ){
+                                        $jobtitle = $row['Name'];
+                                        echo "<option class='jobtitles_option' value=$jobtitle>$jobtitle</option>";
+                                    }
+                                }else { echo mysqli_error($con); }
+                                                    ?>
+                        </select>
+                        <label for="fee" >Fee per hour</label>
+                        <input type="number" name="fee" id="fee" >
+                            </section>
+                    <input type="submit" name="submit" value="Submit">
 
                 </div>
             </form>
@@ -164,6 +232,16 @@
     </article>
 
   <script type="text/javascript" src="togglesubmenu.js"></script>
+
+  <script>
+        $("#program").val($(".jobtitles_option").first().val());
+
+        document.getElementById("job").addEventListener("blur", () => { 
+            console.log($("#job").val())
+           if($("#job").val() === "Trainer" || $("#job").val() === "TRAINER" ||  $("#job").val() === "trainer") 
+           {  $("#for_trainer").show() } else { $("#for_trainer").hide() ; }
+        })
+  </script>
 
 
 </body>
